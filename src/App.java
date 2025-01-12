43b303c87e2cb0e92e39dcffe7208f185a136dd3
JosueGallegos <jgallegoss@est.ups.edu.ec>
2025-01-11 23:23:40 -0500

    Ejercicio 3

--- src/App.java
+++ src/App.java
@@ -1,14 +1,44 @@
 
+import java.util.List;
+import main.Ejercicio_03_listLeves.ListLeves;
 import main.Materia.Controllers.ArbolBinario;
 import main.Materia.Controllers.ArbolRecorridos;
+import main.Materia.Controllers.Models.Node;
 
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
-        runArbolBinario();
+        //runArbolBinario();
+        runEjecicio3();
        
 
     }
+
+    public static void runEjecicio3(){
+        ArbolBinario arbol = new ArbolBinario();
+        ListLeves levels = new ListLeves();
+        int[] valores = {40, 20, 60, 10, 30 ,50, 70, 5, 15, 55};
+          for(int i = 0; i < valores.length; i++){
+               arbol.insert(valores[i]);
+         }
+         arbol.printTree();
+         List<List<Node>> lista = levels.listLeves(arbol.getRoot());
+
+         System.out.println("Listas de niveles");
+         for (int i = 0; i < lista.size(); i++) {
+             System.out.print("Nivel " + i + " : ");
+             for (Node node : lista.get(i)) {
+                 System.out.print(node.getValue() + " ");
+             }
+             System.out.println();
+             
+         }
+
+
+       
+    }
+       
+            
     public static void runArbolBinario(){
         ArbolBinario arbol = new ArbolBinario();
         ArbolRecorridos recorridos = new ArbolRecorridos();
--- src/main/Ejercicio_03_listLeves/ListLeves.java
+++ src/main/Ejercicio_03_listLeves/ListLeves.java
@@ -1,15 +1,44 @@
 package main.Ejercicio_03_listLeves;
-
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
-//import org.w3c.dom.Node;
-
+import java.util.Queue;
 import main.Materia.Controllers.Models.Node;
 
 public class ListLeves {
     public List<List<Node>> listLeves(Node root){
-            return null;
+        List<List<Node>> result = new ArrayList<>();
 
+        if (root != null) {
+           
+
+            Queue<Node> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+                List<Node> levelList = new ArrayList<>();
+
+                for (int i = 0; i < levelSize; i++) {
+                    Node node = queue.poll();
+                    levelList.add(node);
+
+                    if (node.getLeft() != null) {
+                        queue.offer(node.getLeft());
+                    }
+
+                    if (node.getRight() != null) {
+                        queue.offer(node.getRight());
+                    }
+
+                    result.add(levelList);
+                }
+
+            }
+            
+        }
+        return result;
     }
+   
     
 }
