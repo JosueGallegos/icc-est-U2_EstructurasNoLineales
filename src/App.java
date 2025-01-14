5bc64a96561324c99c3d5e1ba96c6b089a2d8df7
JosueGallegos <jgallegoss@est.ups.edu.ec>
2025-01-13 23:41:52 -0500

    Arbol AVL

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 
 import java.util.List;
 import main.Ejercicio_03_listLeves.ListLeves;
+import main.Materia.Controllers.ArbolAVL;
 import main.Materia.Controllers.ArbolBinario;
 import main.Materia.Controllers.ArbolRecorridos;
 import main.Materia.Controllers.Models.Node;
@@ -9,7 +10,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
         //runArbolBinario();
-        runEjecicio3();
+        //runEjecicio3();
+        runArbolAVL();
        
 
     }
@@ -17,7 +19,7 @@ public class App {
     public static void runEjecicio3(){
         ArbolBinario arbol = new ArbolBinario();
         ListLeves levels = new ListLeves();
-        int[] valores = {40, 20, 60, 10, 30 ,50, 70, 5, 15, 55};
+        int[] valores = {1,2,3,4,6,7,9};
           for(int i = 0; i < valores.length; i++){
                arbol.insert(valores[i]);
          }
@@ -58,10 +60,21 @@ public class App {
         recorridos.posOrdenRecursivo(arbol.getRoot());
         System.out.println("\nRecorrido InOrden");
         recorridos.inOrdenRecursivo(arbol.getRoot());
+
+    }
+    public static void runArbolAVL(){
+        //ArbolBinario arbol = new ArbolBinario();
+        ArbolAVL avl = new ArbolAVL();
+        int[] valores = { 10, 20, 15, 24, 9, 8, 21, 23, 50, 25 };
+
         
 
+        for (int value : valores) {
+            avl.insert(value);
+        }
+        
 
-
+        
 
     }
 
--- src/main/Materia/Controllers/Models/Node.java
+++ src/main/Materia/Controllers/Models/Node.java
@@ -4,11 +4,11 @@ public class Node {
     private int value;
     private Node left;
     private Node right;
+    private int height;
 
     public Node(int value){
         this.value = value;
-        this.left = null;
-        this.right = null;
+       
 
     }
     
@@ -39,6 +39,17 @@ public class Node {
     public Node getRight() {
         return right;
     }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
     
 
 
